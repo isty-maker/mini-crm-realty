c03128399548003061601393f4150e6b78de2043
isty-maker <isty240@gmail.com>
2025-10-15 20:23:04 +0700

    Add startup auto migrate helper and panel debug fallback

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
 
+_AUTO_MIGRATED = False
+
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        global _AUTO_MIGRATED
+        if _AUTO_MIGRATED:
+            return
+        try:
+            from .utils.auto_migrate import auto_migrate_once_if_needed
+            auto_migrate_once_if_needed()
+            _AUTO_MIGRATED = True
+        except Exception:
+            # never fail app loading because of auto-migrate
+            pass
